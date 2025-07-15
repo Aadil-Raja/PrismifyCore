e6342d77596dfd8f8a75bc4229520bc02bbb4fb1
Armughan-Ather <armughan.ather83@gmail.com>
2025-07-15 14:11:47 +0500

    minor change

--- Prismify-web/src/ProtectedRoute.jsx
+++ Prismify-web/src/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { useAuth } from './context/AuthContext.jsx';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
+  return isAuthenticated ? children : <Navigate to="/admin/login" replace />;
 };
 
 export default ProtectedRoute;
